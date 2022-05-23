c643ee72b5e69d9b8aee0adaa7f7557ad4e9079b
genwhitt <genwhitt@gmail.com>
2022-05-23 15:54:53 -0400

    ATMO-42172 (wipe out all content files except RN, docs-test)

--- ag/assets/2021_plat/rest/platform/js/toggle.js
+++
@@ -1,8 +0,0 @@
-$(document ).ready(function() {
-    $(".clickable").click(function(event) {
-    	var target = event.currentTarget.attributes["data-target"];
-    	if (target) {
-    		$("#"+target.value).slideToggle("fast");
-    	}
-    });
-});
